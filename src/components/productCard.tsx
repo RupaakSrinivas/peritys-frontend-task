4e799edba903ed57dcc0fef9bfcd87ce80be95da
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-03-28 19:59:43 +0530

    fix: deploy issue

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -18,13 +18,13 @@ export default function Card({ item }: { item: ProductResp }) {
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-        <Image
-            src={item.image}
-            alt={item.title}
-            width={270}
-            height={250}
-            className="rounded-md"
-        />
+      <Image
+        src={item.image}
+        alt={item.title}
+        width={270}
+        height={250}
+        className="rounded-md w-[270px] h-[250px] object-cover"
+      />
     </a>
   );
 }
